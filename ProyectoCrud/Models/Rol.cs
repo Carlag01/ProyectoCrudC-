67f1b689388e3cf8b4ff9fcf2ab856a6d894af41
carla constanza <Carlagon64@gmail.com>
2022-08-29 16:51:38 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoCrud/Models/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCrud.Models
+{
+    public enum Rol
+    {
+        Administrador = 1,
+        Profesor = 2
+
+    }
+}
---
+++ ProyectoCrud/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCrud.Models
+{
+    public class Usuarios
+    {
+        public string Nombres { get; set; }
+        public string Apellido { get; set; }
+        public string Rut { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public Rol IdRol { get; set; }
+
+       
+    }
+}
